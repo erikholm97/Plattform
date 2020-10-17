82766d554e807e4c8debc3a56c33a5cf64f8407d
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2020-10-17 14:04:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Plattform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
